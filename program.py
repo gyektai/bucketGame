2127debdb8c520c01bdc1fd395d5aa7b368e32dd
gyektai-brown <gideon_yektai@brown.edu>
2021-12-15 20:57:53 -0500

    Add comments

--- program.py
+++ program.py
@@ -3,6 +3,7 @@ import os
 
 def approval_program():
 
+    # initialize variables to make code easier to read
     sender = Txn.sender()
     creator = Global.creator_address()
     args = Txn.application_args
@@ -26,6 +27,8 @@ def approval_program():
     in_payout = current_time >= end_time
     can_closeout = current_time >= closing_time
 
+    # initialize global variables
+    # StartTime and EndTime will be used more later
     handle_creation = Seq(
         App.globalPut(Bytes("CreationTime"), Global.latest_timestamp()),
         App.globalPut(Bytes("StartTime"), start_time), # int 1 is bad here. need time
@@ -35,29 +38,39 @@ def approval_program():
         Int(1)
     )
 
+    # set initial local variables to 0 for opt in
     handle_optin = Seq(
         App.localPut(sender, Bytes("A"), Int(0)),
         App.localPut(sender, Bytes("B"), Int(0)),
         Int(1)
     )
 
+    # only can closeout after people have a long time to withdraw
     handle_closeout = If(can_closeout, Return(sender == creator), Reject())
 
+    # can't update
     handle_updateapp = Reject()
 
+    # only can delete if everything's over
     handle_deleteapp = If(can_closeout, Approve(), Reject())
 
+    # add's the bet amount to global A variable
     make_a_bet = Seq(
         App.globalPut(Bytes("A"), a_amt + bet_amt),
         App.localPut(sender, Bytes("A"), a_wagered + bet_amt),
         Int(1)
     )
+
+    # add's the bet amount to global B variable
     make_b_bet = Seq(
         App.globalPut(Bytes("B"), b_amt + bet_amt),
         App.localPut(sender, Bytes("B"), b_wagered + bet_amt),
         Int(1)
     )
 
+    # create transaction to pay out winnings to A betters if A wins
+    # set local state to 0 for no double payouts
+    # transactions fees not dealt with properly
     a_payout = Seq(
         Assert(a_wagered > Int(0)),
         InnerTxnBuilder.Begin(),
@@ -70,6 +83,9 @@ def approval_program():
         App.localPut(sender, Bytes("A"), Int(0)),
         Int(1)
     )
+    # create transaction to pay out winnings to B betters if B wins
+    # set local state to 0 for no double payouts
+    # transactions fees not dealt with properly
     b_payout = Seq(
         Assert(b_wagered > Int(0)),
         InnerTxnBuilder.Begin(),
@@ -83,6 +99,9 @@ def approval_program():
         Int(1)
     )
 
+    # returns funds if there is a tie
+    # sets both local states to 0 for no double payouts
+    # transactions fees not dealt with properly
     equal_payout = Seq(
         Assert(a_wagered + b_wagered > Int(1000)),
         InnerTxnBuilder.Begin(),
@@ -97,6 +116,8 @@ def approval_program():
         Int(1)
     )
 
+    # make sure first transaction was payment to the contract account
+    # set the bet to the side in the args array
     play = Seq(
         Assert(Gtxn[0].receiver() == contract_addr),
         Assert(Gtxn[0].type_enum() == TxnType.Payment),
@@ -106,21 +127,19 @@ def approval_program():
         )
     )
 
+    # decide which side won and make the payout 
     payout = Cond(
         [a_amt < b_amt, a_payout],
         [a_amt > b_amt, b_payout],
         [a_amt == b_amt, equal_payout],
     )
     
-
+    # for application calls, if betting is taking place, then in game
+    # otherwise if it's too late then let people withdraw
     handle_noop = Cond(
         [in_game, play],
         [in_payout, payout],
     )
-
-
-
-
 
 
     program = Cond(
